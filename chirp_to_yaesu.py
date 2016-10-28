60409ce1733f8928d4e2f37d64dd6a3f1fb5583a
Emre Saglam <emresaglam@gmail.com>
2016-10-27 18:26:02 -0700

    Better CSV writing.

--- chirp_to_yaesu.py
+++ chirp_to_yaesu.py
@@ -1,43 +1,63 @@
 import csv
 import argparse
 
+def addEmptyLine(foo, lineNumber):
+    templine = str(lineNumber) + ",,,,,,,,,,,,,,0,,0"
+    foo.append(templine.split(","))
+    return foo
+
+# Adding the command line flags
 parser = argparse.ArgumentParser(description="This tool converts a chirp csv file to a Yaesu importable csv file")
 parser.add_argument('--input', '-i', required = True)
 parser.add_argument('--output', '-o', default="Yaesu-import.csv")
 args = parser.parse_args()
 
-
 ftline = []
+chirpFile = []
 numlines = 0
-with open(args.input) as csvfile:
+inputFile = args.input
+outputFile = args.output
+
+# Open the Chirp file and create the Yaesu formatted array
+with open(inputFile) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         if row["Tone"] == "Tone":
-                numlines = int(row['Location'])+1
-                ftline.append(str(numlines))
-                ftline.append(row['Frequency'])
-                ftline.append('')
-                ftline.append(row['Offset'])
-                ftline.append(row['Duplex'] + "RPT")
-                ftline.append(row['Mode'])
-                ftline.append(row['Name'])
-                ftline.append("TONE ENC")
-                ftline.append(row['rToneFreq'] + " Hz")
-                ftline.append(row['DtcsCode'])
-                ftline.append("1500 Hz")
-                ftline.append("HIGH")
-                ftline.append("OFF")
-                ftline.append("25.0KHz")
-                ftline.append("0")
-                ftline.append(row['Comment'])
-                ftline.append("0")
+            numlines = int(row['Location'])+1
+            ftline.append(str(numlines))
+            ftline.append(row['Frequency'])
+            ftline.append('')
+            ftline.append(row['Offset'])
+            ftline.append(row['Duplex'] + "RPT")
+            ftline.append(row['Mode'])
+            ftline.append(row['Name'])
+            ftline.append("TONE ENC")
+            ftline.append(row['rToneFreq'] + " Hz")
+            ftline.append(row['DtcsCode'])
+            ftline.append("1500 Hz")
+            ftline.append("HIGH")
+            ftline.append("OFF")
+            ftline.append("25.0KHz")
+            ftline.append("0")
+            ftline.append(row['Comment'])
+            ftline.append("0")
 
-                print ",".join(ftline)
-                ftline = []
+            chirpFile.append(ftline)
+            ftline = []
         else:
             # If it's not Tone, Don't do anything now, just add an empty line
             # This will have to handle DCS stuff one day.
-            print str(numlines+1) + ",,,,,,,,,,,,,,0,,0"
+            numlines += 1
+            chirpFile = addEmptyLine(chirpFile, numlines)
+
+# For some reason Yaesu import CSV file expects 500 lines,
+# filling the rest with empty lines here
 
 for line in range(numlines+1, 501):
-    print str(line) + ",,,,,,,,,,,,,,0,,0"
+    chirpFile = addEmptyLine(chirpFile, line)
+
+# Writing the file to Yaesu importable CSV file.
+with open(outputFile, "w") as csvWriter:
+    writer = csv.writer(csvWriter, delimiter=",")
+    for line in chirpFile:
+        writer.writerow(line)
